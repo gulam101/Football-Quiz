fb122b1a677fdd819aca3c0f8a05e352cd7a626a
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-23 06:42:02 +0000

    Start Game Properties

--- Football Quiz/Start Game.Designer.cs
+++ Football Quiz/Start Game.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Start_Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Start_Game";
+            this.MaximizeBox = false;
+            this.Name = "Start_Game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Start Game";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
